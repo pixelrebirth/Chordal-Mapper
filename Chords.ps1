1e7c87ecaf6e9fb77238c7d8df304a512b13070d
Kelcey Vehanen <KelceyVehanen@corp.fairisaac.com>
2017-04-12 22:31:50 -0700

    Working

--- Chords.ps1
+++ Chords.ps1
@@ -1,57 +1,17 @@
-function Set-FlatSharp {
-    param ($inputLetter,$after)
+param (
+    [cmdletbinding()]
+    [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$primary_mode,
+    [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$secondary_mode
+)
 
-    if ($inputLetter -cmatch "b$"){return $after + "b"}
-    if ($inputLetter -cmatch "\#$"){return $after + "#"}
-    return $after
-}
-
-$primary_mode = "Ionian"
-$secondary_mode = "Dorian"
-
+. $PSScriptRoot/functions.ps1
 $chordal_data = Import-Csv -Path "Chords by Mode - Chordal Notes.csv"
+
 $base_chord = $chordal_data | Where-Object {$_.mode -eq "Ionian"}
 $primary_chord = $chordal_data | Where-Object {$_.mode -eq $primary_mode}
 $secondary_chord = $chordal_data | Where-Object {$_.mode -eq $secondary_mode}
 
-$AtoG_All = ("A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G")
-$CtoB_All = ("C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G", "A", "B")
-# $GtoA = ("G", "F", "E", "D", "C", "B", "A")
-# $CtoD = ("C", "B", "A", "G", "F", "E", "D")
-# $CtoD_All = ("C", "B", "A", "G", "F", "E", "D", "C", "B", "A", "G", "F", "E", "D")
-
-1..7 | foreach-object {
-    $chord_num = $_
-    $interval = $_ - 1
-    $array = $primary_chord."chord_$chord_num".split('-')
-    $after = $AtoG_All[$interval+$primary_chord.Offset]
-    $array[0] = $after
-    $after = $AtoG_All[$interval+2+$primary_chord.Offset]
-    $array[1] = $after
-    $after = $AtoG_All[$interval+4+$primary_chord.Offset]
-    $array[2] = $after
-    $after = $AtoG_All[$interval+6+$primary_chord.Offset]
-    $array[3] = $after
-    $string = $array -join "-"
-    $primary_chord."chord_$chord_num" = $string
-}
-$primary_chord
+Convert-ChordalMap -chord $primary_chord -primary $true
 
 $diff = $primary_chord.Offset - $secondary_chord.Offset
-
-1..7 | foreach-object {
-    $chord_num = $_
-    $interval = $_ - 1
-    $array = $secondary_chord."chord_$chord_num".split('-')
-    $after = $AtoG_All[$interval+$diff+$secondary_chord.Offset]
-    $array[0] = Set-FlatSharp -after $after -inputLetter $array[0]
-    $after = $AtoG_All[$interval+$diff+2+$secondary_chord.Offset]
-    $array[1] = Set-FlatSharp -after $after -input $array[1]
-    $after = $AtoG_All[$interval+$diff+4+$secondary_chord.Offset]
-    $array[2] = Set-FlatSharp -after $after -input $array[2]
-    $after = $AtoG_All[$interval+$diff+6+$secondary_chord.Offset]
-    $array[3] = Set-FlatSharp -after $after -input $array[3]
-    $string = $array -join "-"
-    $secondary_chord."chord_$chord_num" = $string
-}
-$secondary_chord
+Convert-ChordalMap -chord $secondary_chord -diff $diff
---
+++ Functions.ps1
@@ -0,0 +1,39 @@
+function Set-FlatSharp {
+    param ($inputLetter,$after)
+
+    if ($inputLetter -cmatch "b$"){return $after + "b"}
+    if ($inputLetter -cmatch "\#$"){return $after + "#"}
+    return $after
+}
+
+function Convert-ChordalMap {
+    param (
+        $chord,
+        $diff = 0,
+        $primary
+    )
+
+    $Notes = ("A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G")
+    1..7 | foreach-object {
+        $chord_num = $_
+        $interval = $_ - 1
+        $array = $chord."chord_$chord_num".split('-')
+
+        $count = 0
+        0..3 | foreach-object {
+            $after = $Notes[$interval+$count+$diff+$chord.Offset]
+
+            if ($primary){
+                $array[$_] = $after
+            }
+            else {
+                $array[$_] = Set-FlatSharp -after $after -inputLetter $array[$_]
+            }
+
+            $count += 2
+        }
+        $string = $array -join "-"
+        $chord."chord_$chord_num" = $string
+    }
+    return $chord
+}
