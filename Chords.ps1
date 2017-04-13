b7f7f76028488262ff258998b2bcd427390523f7
Kelcey Vehanen <KelceyVehanen@corp.fairisaac.com>
2017-04-12 22:41:17 -0700

    Fixed mood vs mode input sets

--- Chords.ps1
+++ Chords.ps1
@@ -1,15 +1,33 @@
 param (
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName='Modes')]
+
+    [Parameter(ParameterSetName="Modes")]
     [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$primary_mode,
-    [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$secondary_mode
+    [Parameter(ParameterSetName="Modes")]
+    [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$secondary_mode,
+
+    [Parameter(ParameterSetName="Moods")]
+    [ValidateSet('Very bright, Congradulatory','Love songs, uplifting, happy','Optimistic Realist','Serious/Attentive',
+        'Sad/Depressed','Exotic/Different','Unsettling and Unruly')]$mood_primary,
+    [Parameter(ParameterSetName="Moods")]
+    [ValidateSet('Very bright, Congradulatory','Love songs, uplifting, happy','Optimistic Realist','Serious/Attentive',
+        'Sad/Depressed','Exotic/Different','Unsettling and Unruly')]$mood_secondary
 )
 
 . $PSScriptRoot/functions.ps1
-$chordal_data = Import-Csv -Path "Chords by Mode - Chordal Notes.csv"
 
+$chordal_data = Import-Csv -Path "Chords by Mode - Chordal Notes.csv"
 $base_chord = $chordal_data | Where-Object {$_.mode -eq "Ionian"}
-$primary_chord = $chordal_data | Where-Object {$_.mode -eq $primary_mode}
-$secondary_chord = $chordal_data | Where-Object {$_.mode -eq $secondary_mode}
+
+if ($PScmdlet.ParameterSetName -eq "Modes"){
+    $primary_chord = $chordal_data | Where-Object {$_.mode -eq $primary_mode}
+    $secondary_chord = $chordal_data | Where-Object {$_.mode -eq $secondary_mode}
+}
+
+if ($PScmdlet.ParameterSetName -eq "Moods"){
+    $primary_chord = $chordal_data | Where-Object {$_.mood -eq $mood_primary}
+    $secondary_chord = $chordal_data | Where-Object {$_.mood -eq $mood_secondary}
+}
 
 Convert-ChordalMap -chord $primary_chord -primary $true
 
