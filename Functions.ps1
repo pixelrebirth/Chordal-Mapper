a6730f3ef12592d6701ec12deb51fec740b09c80
Kelcey Vehanen <KelceyVehanen@corp.fairisaac.com>
2017-04-13 00:07:33 -0700

    Working Well, Change Feeder Data

--- Functions.ps1
+++ Functions.ps1
@@ -1,44 +1,20 @@
-function Set-FlatSharp {
-    param ($inputLetter,$after)
-
-    if ($inputLetter -cmatch "b$"){return $after + "b"}
-    if ($inputLetter -cmatch "\#$"){return $after + "#"}
-    return $after
-}
-
 function Convert-ChordalMap {
-    param (
-        $chord,
-        $diff = 0,
-        $primary
-    )
+    param ($chord,$diff = 0)
 
-    $Notes = ("A", "B", "C", "D", "E", "F", "G",
-        "A", "B", "C", "D", "E", "F", "G",
-        "A", "B", "C", "D", "E", "F", "G"
-    )
-
-    $Notes_Steps = ('A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab',
+    $Notes = ('A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab',
+        'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab',
         'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'
     )
 
+    $chord_base = [int]$chord.Chord_1.split('-')[0] + [int]$chord.Offset - 3 + $diff
+
     1..7 | foreach-object {
         $chord_num = $_
-        $interval = $_ - 1
         $array = $chord."chord_$chord_num".split('-')
 
-        $count = 0
         0..3 | foreach-object {
-            $after = $Notes[$interval+$count+$diff+$chord.Offset]
-
-            if ($primary){
-                $array[$_] = $after
-            }
-            else {
-                $array[$_] = Set-FlatSharp -after $after -inputLetter $array[$_]
-            }
-
-            $count += 2
+                $note_step = [int]$chord_base + [int]$array[$_]
+                $array[$_] = $notes[$note_step]
         }
         $string = $array -join "-"
         $chord."chord_$chord_num" = $string
