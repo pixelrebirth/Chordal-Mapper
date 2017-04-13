1a6eb337593bbb07747dd6e24e14e94fcb8b8ba9
Kelcey Vehanen <KelceyVehanen@corp.fairisaac.com>
2017-04-13 08:42:57 -0700

    Fixed chordal data sheet

--- Chords.ps1
+++ Chords.ps1
@@ -1,38 +1,40 @@
-param (
-    [cmdletbinding(DefaultParameterSetName='Modes')]
+function Get-ChordalRelationship {
+    param (
+        [cmdletbinding(DefaultParameterSetName='Modes')]
 
-    [Parameter(ParameterSetName="Modes")]
-    [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$primary_mode,
-    [Parameter(ParameterSetName="Modes")]
-    [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$secondary_mode,
+        [Parameter(ParameterSetName="Modes")]
+        [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$primary_mode,
+        [Parameter(ParameterSetName="Modes")]
+        [ValidateSet('Lydian','Ionian','Mixolydian','Dorian','Aeolian','Phrygian','Locrian')]$secondary_mode,
 
-    [Parameter(ParameterSetName="Moods")]
-    [ValidateSet('Very bright, Congradulatory','Love songs, uplifting, happy','Optimistic Realist','Serious/Attentive',
-        'Sad/Depressed','Exotic/Different','Unsettling and Unruly')]$mood_primary,
-    [Parameter(ParameterSetName="Moods")]
-    [ValidateSet('Very bright, Congradulatory','Love songs, uplifting, happy','Optimistic Realist','Serious/Attentive',
-        'Sad/Depressed','Exotic/Different','Unsettling and Unruly')]$mood_secondary
-)
+        [Parameter(ParameterSetName="Moods")]
+        [ValidateSet('Very bright, Congradulatory','Love songs, uplifting, happy','Optimistic Realist','Serious/Attentive',
+            'Sad/Depressed','Exotic/Different','Unsettling and Unruly')]$mood_primary,
+        [Parameter(ParameterSetName="Moods")]
+        [ValidateSet('Very bright, Congradulatory','Love songs, uplifting, happy','Optimistic Realist','Serious/Attentive',
+            'Sad/Depressed','Exotic/Different','Unsettling and Unruly')]$mood_secondary
+    )
 
-. $PSScriptRoot/functions.ps1
+    . $PSScriptRoot/functions.ps1
 
-$chordal_data = Import-Csv -Path "Chords by Mode - Step Sheet.csv"
-$base_chord = $chordal_data | Where-Object {$_.mode -eq "Ionian"}
+    $chordal_data = Import-Csv -Path "Chords by Mode - Step Sheet.csv"
+    $base_chord = $chordal_data | Where-Object {$_.mode -eq "Ionian"}
 
-if ($PScmdlet.ParameterSetName -eq "Modes"){
-    $primary_chord = $chordal_data | Where-Object {$_.mode -eq $primary_mode}
-    $secondary_chord = $chordal_data | Where-Object {$_.mode -eq $secondary_mode}
+    if ($PScmdlet.ParameterSetName -eq "Modes"){
+        $primary_chord = $chordal_data | Where-Object {$_.mode -eq $primary_mode}
+        $secondary_chord = $chordal_data | Where-Object {$_.mode -eq $secondary_mode}
+    }
+
+    if ($PScmdlet.ParameterSetName -eq "Moods"){
+        $primary_chord = $chordal_data | Where-Object {$_.mood -eq $mood_primary}
+        $secondary_chord = $chordal_data | Where-Object {$_.mood -eq $mood_secondary}
+    }
+
+    $output = @()
+    $output += Convert-ChordalMap -chord $primary_chord -primary $true
+
+    $diff = $primary_chord.Offset - $secondary_chord.Offset
+    $output += Convert-ChordalMap -chord $secondary_chord -diff $diff
+
+    return $output | Format-Table
 }
-
-if ($PScmdlet.ParameterSetName -eq "Moods"){
-    $primary_chord = $chordal_data | Where-Object {$_.mood -eq $mood_primary}
-    $secondary_chord = $chordal_data | Where-Object {$_.mood -eq $mood_secondary}
-}
-
-$output = @()
-$output += Convert-ChordalMap -chord $primary_chord -primary $true
-
-$diff = $primary_chord.Offset - $secondary_chord.Offset
-$output += Convert-ChordalMap -chord $secondary_chord -diff $diff
-
-return $output | Format-Table
