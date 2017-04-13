5fc21100b221f36b8eb5c53216563ec9567fd653
Kelcey Vehanen <KelceyVehanen@corp.fairisaac.com>
2017-04-13 11:38:56 -0700

    Put down next chord relationships

---
+++ Set-NextChord.ps1
@@ -0,0 +1,21 @@
+function Set-NextChord {
+    3-6-4/2-5/7-1
+
+    1 - any
+    7.1 - 1 backdoor
+    7 - 3
+    3 - 6
+    3 - 4
+    6 - 4
+    6 - 2
+    2 - 5
+    2 - 7
+    4 - 2
+    4 - 5
+    4 - 7
+    4 - 1 plegal
+    5 - 6 deceptive
+    5 - 1 perfect
+    5 - 7 deceptive
+    7 - 6
+}
