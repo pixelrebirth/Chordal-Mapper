c0577a2be486cccce679d5c8d8184ab33758acae
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2017-04-19 23:08:23 -0700

    Fixed Get-KeySignature to be accurate

--- Get-KeySignature.ps1
+++ Get-KeySignature.ps1
@@ -1,58 +1,96 @@
 function Get-KeySignature {
     param (
         [CmdletBinding()]
-        [ValidateSet("C","G", "D", "A", "E", "B", "Cb", "Gb", "F#", "Db", "C#", "Ab", "Eb", "Bb", "F")]$root_key,
+        [ValidateSet("A#","B#", "D#", "E#", "Fb", "G#", "C","G", "D", "A", "E", "B", "Cb", "Gb", "F#", "Db", "C#", "Ab", "Eb", "Bb", "F")]$root_key,
         [ValidateSet("Major","Minor","Dim")]$scale_type
     )
 
     $chordal_data = Import-Csv -Path "Chords by Mode - Step Sheet.csv"
     $modes = $chordal_data.Mode
-    $root_notes = @("C","G", "D", "A", "E", "B", "Cb", "Gb", "F#", "Db", "C#", "Ab", "Eb", "Bb", "F")
 
-    $root_number = switch ($root_key) {
-        "C" {"0"}
-        "G" {"1"}
-        "D" {"2"}
-        "A" {"3"}
-        "E" {"4"}
-        "B" {"5"}
-        "Cb" {"6"}
-        "Gb" {"7"}
-        "F#" {"8"}
-        "Db" {"9"}
-        "C#" {"10"}
-        "Ab" {"11"}
-        "Eb" {"12"}
-        "Bb" {"13"}
-        "F" {"14"}
+    if ($scale_type -eq "Major"){
+        $root_number = switch ($root_key) {
+            "C" {0}
+            "G" {1}
+            "D" {2}
+            "A" {3}
+            "E" {4}
+            "B" {5}
+            "F#" {6}
+            "C#" {7}
+            "Cb" {8}
+            "Gb" {9}
+            "Db" {10}
+            "Ab" {11}
+            "Eb" {12}
+            "Bb" {13}
+            "F" {14}
+            default {-1}
+        }
     }
     
-    $root_offset = switch ($scale_type){
-        "Major" {0}
-        "Dim" {-5}
-        "Minor" {-3}
+    if ($scale_type -eq "Minor"){
+        $root_number = switch ($root_key) {
+            "A" {0}
+            "E" {1}
+            "B" {2}
+            "F#" {3}
+            "C#" {4}
+            "G#" {5}
+            "D#" {6}
+            "A#" {7}
+            "Ab" {8}
+            "Eb" {9}
+            "Bb" {10}
+            "F" {11}
+            "C" {12}
+            "G" {13}
+            "D" {14}
+            default {-1}
+        }
     }
 
-    $key_diff = [int]$root_number + [int]$root_offset
+    if ($scale_type -eq "Dim"){
+        $root_number = switch ($root_key) {
+            "B" {0}
+            "F#" {1}
+            "C#" {2}
+            "G#" {3}
+            "D#" {4}
+            "A#" {5}
+            "E#" {6}
+            "B#" {7}
+            "Bb" {8}
+            "F" {9}
+            "C" {10}
+            "G" {11}
+            "D" {12}
+            "A" {13}
+            "E" {14}
+            default {-1}
+        }
+    }
+
+    $key_diff = $root_number
     if ($key_diff -gt 14){$key_diff = $key_diff - 14}
 
-    Write-Verbose "Key_diff: $key_diff, $root_number, $root_offset"
     $key_signature =  switch ($key_diff) {
-        "0" {""}
+        "0" {"NaN"}
         "1" {"F#"}
         "2" {"F#,C#"}
         "3" {"F#,C#,G#"}
         "4" {"F#,C#,G#,D#"}
         "5" {"F#,C#,G#,D#,A#"}
-        "6" {"Bb,Eb,Ab,Db,Gb,Cb,Fb"}
-        "7" {"Bb,Eb,Ab,Db,Gb,Cb"}
-        "8" {"F#,C#,G#,D#,A#,E#"}
-        "9" {"Bb,Eb,Ab,Db,Gb"}
-        "10" {"F#,C#,G#,D#,A#,E#,B#"}
+        "6" {"F#,C#,G#,D#,A#,E#"}
+        "7" {"F#,C#,G#,D#,A#,E#,B#"}
+        "8" {"Bb,Eb,Ab,Db,Gb,Cb,Fb"}
+        "9" {"Bb,Eb,Ab,Db,Gb,Cb"}
+        "10" {"Bb,Eb,Ab,Db,Gb"}
         "11" {"Bb,Eb,Ab,Db"}
         "12" {"Bb,Eb,Ab"}
         "13" {"Bb,Eb"}
         "14" {"Bb"}
+        default {"Key: $root_key not on circle of fifths for scale: $scale_type"}
     }
 
     $key_signature.split(",") | Sort
