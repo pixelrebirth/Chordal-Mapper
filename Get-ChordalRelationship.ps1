5d8b54379cf8b2a7d764a04d0d2e1ded47913163
Kelcey Vehanen <KelceyVehanen@corp.fairisaac.com>
2017-04-24 15:55:49 -0700

    Made changes to let the script run in VSCode and shell

--- Get-ChordalRelationship.ps1
+++ Get-ChordalRelationship.ps1
@@ -1,11 +1,8 @@
 # # function Get-ChordalRelationship {
-#     param (
-#         [ValidateSet("A#","B#", "C#", "D#", "E#", "F#", "G#", "A","B", "C", "D", "E", "F", "G", "Ab","Bb", "Cb", "Db", "Eb", "Fb", "Gb")]$root_key,
-#         [ValidateSet("Major","Minor","Dim")]$scale_type
-#     )
-
-    $root_key = "Gb"
-    $scale_type = 'Major'
+    param (
+        [Parameter(Mandatory=$true)][ValidateSet("A#","B#", "C#", "D#", "E#", "F#", "G#", "A","B", "C", "D", "E", "F", "G", "Ab","Bb", "Cb", "Db", "Eb", "Fb", "Gb")]$root_key,
+        [Parameter(Mandatory=$true)][ValidateSet("Major","Minor","Dim")]$scale_type
+    )
 
     import-module .\Chordal-Mapper.psd1 -force
 
@@ -20,5 +17,5 @@
         $chordal_map = Convert-ChordalMap -scale $scale
     }
     catch {$error.exception.message ; exit 1}
-    $chordal_map | Format-Table
+    $chordal_map | select Chord_1,Chord_2,Chord_3,Chord_4,Chord_5,Chord_6,Chord_7,Mode,Mood | Format-Table
 # }
